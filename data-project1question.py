ff52185d778c7475e7a785cfe7d7ad82a20b8adf
Selin Mayli <selin_aleyna99@hotmail.com>
2021-09-13 15:06:25 +0300

    kodda değişiklik yapıldı

--- data-project1question.py
+++ data-project1question.py
@@ -2,22 +2,11 @@ import pandas as pd
 
 df=pd.read_parquet('./ornek_dosya.parquet')
 
-result=df
-result=result.astype({"latitude":str,"longitude":str})#change colums type
+result=df.astype({"latitude":str,"longitude":str})#change colums type
 
-result["lat_long"] = result["latitude"] + result["longitude"]#merge columns 
-
-
-result =result[result.duplicated("lat_long")==False]#control duplicate or not 
-result1 = result['lat_long'].value_counts()#count values
-result2 =result.head(3)#return first 3 rows 
-
-print (result2)#print result2
+result["lat_long"] = result["latitude"] +','+ result["longitude"]#merge columns 
 
 
-result3 =result[(result['lat_long']=='41.020928.9217') |(result['lat_long']=='41.064428.9228') |(result['lat_long']=='41.053828.9374')]['id'].value_counts()
-#control id 
-# result3=result3.head(9)
+result =result[result.duplicated("lat_long")]["lat_long"].value_counts()#control duplicate or not 
 
-print(result3)
-
+print (result)#print result
--- data-project2.question.py
+++ data-project2.question.py
@@ -5,6 +5,10 @@ import pandas as pd
 
 df=pd.read_parquet('./ornek_dosya.parquet')
 
-result =df[(df.duplicated(['timestamp'])) & (df.duplicated(['latitude','longitude'])==False)& (df.duplicated('id')==False)]['id']#control id for this situations
-                                          
-print (result)
+result=df.astype({"latitude":str,"longitude":str})#change colums type
+
+result["lat_long"] = result["latitude"] +','+ result["longitude"]#merge columns 
+
+result_id =result[(result.duplicated(['timestamp'])) & (result.duplicated("lat_long")==False)]['id'].value_counts()#control id for this situations
+                                        
+print (result_id)
